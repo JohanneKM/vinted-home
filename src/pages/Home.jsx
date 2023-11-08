b806e22896ee26faff6ff04c5f50e3a9b14de240
Johanne Khan Malek <johannekhanmalek@johannes-mbp.lan>
2023-11-08 16:41:07 +0100

    with image hero

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,21 +4,29 @@ import Header from "../components/Header";
 
 const Home = ({ data }) => {
   return (
-    <main>
+    <div className="home">
       <Header />
+
+      <div className="hero">
+        <img src="../src/assets/img/img-hero.jpg" alt="image hero" />
+      </div>
+
       <h1>Je suis sur la page Home</h1>
       <Link to="/offers">Cliquez ici pour naviguer vers la page Offers</Link>
+      <h2>Articles Populaires</h2>
 
       <div className="offers">
         {data.offers.map((elem) => {
           return (
             <div key={elem.product_image.asset_id}>
               <img className="offer-img" src={elem.product_image.url} alt="" />
+              <p> {elem.product_price} €</p>
+              <p> {elem.product_name} </p>
             </div>
           );
         })}
       </div>
-    </main>
+    </div>
   );
 };
 
