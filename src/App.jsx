7802249dad41680c2308b927ae402bc2f4d58199
Johanne Khan Malek <johannekhanmalek@johannes-mbp.lan>
2023-11-08 15:04:45 +0100

    nav to offer page ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
-import { useState } from "react";
-
 import "./App.css";
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import axios from "axios";
+
+import Home from "./pages/Home";
+import Offer from "./pages/Offer";
 
 function App() {
-  return <></>;
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        "https://lereacteur-vinted-api.herokuapp.com/offers"
+      );
+      setData(response.data);
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/offers" element={<Offer />} />
+
+          <Route path="*" element={<p>All</p>} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
